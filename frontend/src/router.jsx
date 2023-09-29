eb0bbaa0d9662d7a32c35f6a2aa9542324d56e90
Thimet <92925044+Thimet@users.noreply.github.com>
2023-09-29 23:54:22 +0530

    Created login page

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,11 +1,16 @@
 // Path: frontend/src/router.js
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../src/pages/Login.jsx"
 
 const routes = [
 	{
 		path: "/",
 		element: <div>Landing</div>,
 	},
+	{
+		path: "/login",
+		element: <Login />
+	}
 ];
 
 const router = createBrowserRouter(routes);
