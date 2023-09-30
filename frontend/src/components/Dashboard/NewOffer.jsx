8928a3a9aef192ad0ea555dcacc846bf0c168caa
kavishka-sulakshana <kss20001218@gmail.com>
2023-09-30 16:12:55 +0530

    Responsive dashboard

--- frontend/src/components/Dashboard/NewOffer.jsx
+++ frontend/src/components/Dashboard/NewOffer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NewOffer = ({ children, title, buttonText }) => {
     return (
-        <div className='flex flex-col h-44 p-5 w-44 bg-slate-900/10 rounded-lg justify-between mx-2 border-2 hover:border-[#001F54]/30 border-transparent'>
+        <div className='flex flex-col  md:h-44 p-5 md:w-44 h-48 w-48 bg-slate-900/10 rounded-lg justify-between m-2 border-2 hover:border-[#001F54]/30 border-transparent'>
             <h1 className='font-bold'>{title}</h1>
             <div className='flex flex-col my-3'>
                 {children}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,25 +4,26 @@ import NotificationPanel from '../components/Dashboard/NotificationPanel';
 import ProgressContainer from '../components/Dashboard/ProgressContainer';
 import ProgressSet from '../components/Dashboard/ProgressSet';
 import NewOffer from '../components/Dashboard/NewOffer';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-        <section className=' w-[70%] flex flex-col mx-auto' >
+        <section className=' md:w-[70%] w-full flex flex-col mx-auto' >
             <DashNav />
             <NotificationPanel />
-            <div className='flex flex-wrap'>
+            <div className='flex lg:flex-row flex-col'>
                 <ProgressContainer title={"Data Usage"}>
                     <ProgressSet all={30} precent={60} save={23} title={"Any time Data"} />
                     <ProgressSet all={30} precent={20} save={10} title={"Night time Data"} />
                 </ProgressContainer>
-                <ProgressContainer title={"Voice Packages"}>
+                <ProgressContainer title={"Voice Usage"}>
                     <ProgressSet all={30} precent={60} save={23} title={"Any time Data"} />
                     <ProgressSet all={30} precent={20} save={10} title={"Night time Data"} />
                 </ProgressContainer>
             </div>
-            <div className='flex flex-wrap'>
+            <div className='flex'>
                 <ProgressContainer title={"Offers"}>
-                    <div className='flex justify-center'>
+                    <div className='flex justify-center flex-wrap'>
                         <NewOffer title={"Combo Pack"} buttonText={"Activate"}>
                             <h2>Data : 500 MB</h2>
                             <h2>Duration : 7 Days</h2>
@@ -34,6 +35,9 @@ const Dashboard = () => {
                         <NewOffer title={"Youtube monthly"} buttonText={"Activate"}>
                             <h2>Duration : 30 Days</h2>
                         </NewOffer>
+                    </div>
+                    <div className='flex my-5 md:justify-end justify-center'>
+                        <Link to='/offers' className=''>{'See all >'}</Link>
                     </div>
                 </ProgressContainer>
             </div>
