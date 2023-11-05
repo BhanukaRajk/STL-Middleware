7714365e5af1c9e9ccf44fcb212cf846b1f3c65a
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-11-05 13:50:47 +0530

    UI updates

--- frontend/src/components/Common/Banner.jsx
+++ frontend/src/components/Common/Banner.jsx
@@ -3,13 +3,13 @@ import RoamingImage from "../../assets/images/banners/roaming-banner.jpg";
 // THIS IS THE STATIC BANNER IMAGE IN THE ROAMING PAGE TOP
 const Banner = () => {
     return (
-        <div className="flex flex-row">
-            <div className="flex flex-col">
-                <div>Travel abroad</div>
-                <div>Stay connected wherever you go</div>
+        <div className="flex flex-col w-full">
+            <div className="flex flex-col absolute">
+                <div className="text-5xl font-serif">Travel abroad</div>
+                <div className="text-sm font-serif">Stay connected wherever you go</div>
             </div>
-            <div>
-                <image src={RoamingImage} alt="Roaming Image"></image>
+            <div className="w-full">
+                <img src={RoamingImage} alt="Roaming Image" />
             </div>
         </div>
     );
--- frontend/src/components/Roaming/CountryDetailCard.jsx
+++ frontend/src/components/Roaming/CountryDetailCard.jsx
@@ -1,4 +1,31 @@
 // ROAMING COUNTRY CHARGE DETAILS CARD
+
+
+// const GameData = {
+//     category: {
+//         service: "Games",
+//         description: "Connect with most popular gaming communities and play latest games.",
+//     },
+//     options: [
+//         {
+//             name: "$3 Pack (LKR 981)",
+//             data: "1GB Data",
+//             voice: "30 Minutes Calls",
+//             incoming: "30 Minutes Incoming Calls Send SMS",
+//             time: "24hrs hours",
+//             code: "*91*3#",
+//         },
+//         {
+//             name: "$3 Pack (LKR 1644)",
+//             data: "5GB Data",
+//             voice: "90 Minutes Calls",
+//             incoming: "90 Minutes Incoming Calls Send SMS",
+//             time: "10 Days",
+//             code: "*91*15#",
+//         },
+//     ],
+// };
+
 const CountryDetailCard = () => {
     return (
         <div className="flex flex-col bg-slate-400 p-5">
