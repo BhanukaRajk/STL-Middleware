779350ad0454f5599b8d22535b8b8c93f4731b6b
Binula Rasanjith <binula28@gmail.com>
2023-11-05 18:23:46 +0530

    Add authentication API functions and request interceptor

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,48 @@
+import { apiTelco } from ".";
+
+export const login = async (email, password) => {
+    try {
+        const response = await apiTelco.post('/auth/login', { email, password });
+
+        if (response.status === 200) {
+            console.log(response.data);
+            return response.data.token;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+export const signup = async (firstName, lastName, phoneNo, email, password) => {
+    try {
+        const response = await apiTelco.post('/auth/signup', { firstName, lastName, phoneNo, email, password });
+
+        if (response.status === 201) {
+            return response.data.customer;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+};
+
+export const testAuth = async () => {
+    try {
+        const response = await apiTelco.get('/auth/test');
+
+        if (response.status === 200) {
+            console.log(response.data);
+            return response.data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
