c6b9ae90ae6822e8281e4d46cf57dd4b3efdb7f9
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-11-05 20:16:35 +0530

    Backend frontend connected

--- telco-services/routes/index.js
+++ telco-services/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mainRouter from "./main.router.js";
 import dashboardRouter from "./dashboard.router.js";
 import roamingRouter from "./roaming.router.js";
+import vasRouter from "./vas.router.js";
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/', mainRouter);
 router.use('/dashboard', dashboardRouter)
 router.use('/roaming', roamingRouter)
+router.use('/vas', vasRouter)
 
 export default router;
--- value-added-services/index.js
+++ value-added-services/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import mainRouter from './routes/main.router.js';
 dotenv.config();
@@ -7,10 +8,15 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 3008;
+const mongo_uri = process.env.MONGO_URI;
 
 app.use(cors());
 app.use(express.json());
-app.use('/', mainRouter);
+app.use('/vas', mainRouter);
+
+mongoose.connect(mongo_uri, { dbName: 'telco' })
+    .then(() => console.log('=> Connected to MongoDB'))
+    .catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`VAS Server Listening on port ${port}`);
