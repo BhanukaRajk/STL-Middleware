dc6f70ca677fb4fafe5e92db2485048a713eafba
Binula Rasanjith <binula28@gmail.com>
2023-11-05 18:23:31 +0530

    Add authentication functionality with Passport and JWT

--- telco-services/controllers/main.controller.js
+++ telco-services/controllers/main.controller.js
@@ -2,7 +2,7 @@ import Customer from '../models/customer.model.js';
 import { apiAddOns } from '../api/index.js';
 
 export const init = async (req, res) => {
-    const customers = await Customer.find({})
-    const res = await apiAddOns.get('/');
-    res.json({ message: 'Hello from  Services API' });
+    // const customers = await Customer.find({})
+    // const res = await apiAddOns.get('/');
+    // res.json({ message: 'Hello from  Services API' });
 }
